d743fbfdae87a09a83828c1d34e6550097a9aea2
julu1994 <mrjewel837@gmail.com>
2022-05-18 17:43:59 +0200

    heading component

--- my-portfolio/src/components/header.js
+++ my-portfolio/src/components/header.js
@@ -1,7 +1,21 @@
+import "./header.scss";
 import React from "react";
 
 function Header() {
-    return <div>hello world..</div>;
+    return (
+        <div class="heading">
+            <div class="header_box">
+                <h1>
+                    <span class="heading-part-1">
+                        Hi.. I am Mahamudur Rahamn
+                    </span>
+                    <span class="heading-part-2">
+                        I am a full-stack developer
+                    </span>
+                </h1>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
