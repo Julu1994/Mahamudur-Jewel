34a2e24b32893eb043a5a9a06d83aee35b51bc1f
julu1994 <mrjewel837@gmail.com>
2022-05-19 16:45:42 +0200

    heading animation

--- my-portfolio/src/components/header.js
+++ my-portfolio/src/components/header.js
@@ -1,15 +1,14 @@
 import "./header.scss";
-import Typewriter from "./sub-components/typewriter";
 
 function Header() {
     return (
         <div className="heading">
             <div className="header_box">
                 <h1>
-                    <span className="heading-part-1">
-                        <Typewriter props={"HHi.. I am Mahamudur Rahman"} />
+                    <span className="heading-part-1 animate">
+                        Hi.. I am Mahamudur Rahman
                     </span>
-                    <span className="heading-part-2 animate">
+                    <span className="heading-part-2">
                         I am a full-stack developer
                     </span>
                 </h1>
