5d0c5ae3dcff08b210eab43adcd58e450057fca9
julu1994 <mrjewel837@gmail.com>
2022-05-29 15:32:58 +0200

    completing the footer section

--- my-portfolio/src/Layout/Layout.js
+++ my-portfolio/src/Layout/Layout.js
@@ -6,6 +6,7 @@ import About from "../components/about";
 import Skills from "../components/skills";
 import Contact from "../components/Contact";
 import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 import notePic from "../components/resources/stikynote1.webp";
 import ecommarce from "../components/resources/ecommarceapp.png";
 import fooddelivery from "../components/resources/foodordering2.png";
@@ -152,16 +153,8 @@ function Layout() {
 
             {/* Copyright  */}
             <section className="copyright">
-                <div className="funny-copyright"></div>
                 <div>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 1440 320">
-                        <path
-                            fill="#5001ac"
-                            fillOpacity="0.7"
-                            d="M0,96L26.7,128C53.3,160,107,224,160,218.7C213.3,213,267,139,320,138.7C373.3,139,427,213,480,245.3C533.3,277,587,267,640,250.7C693.3,235,747,213,800,176C853.3,139,907,85,960,101.3C1013.3,117,1067,203,1120,250.7C1173.3,299,1227,309,1280,282.7C1333.3,256,1387,192,1413,160L1440,128L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path>
-                    </svg>
+                    <Footer />
                 </div>
             </section>
         </div>
--- my-portfolio/src/components/footer.js
+++ my-portfolio/src/components/footer.js
@@ -5,16 +5,34 @@ import { AiFillLinkedin, AiFillGithub, AiFillFacebook } from "react-icons/ai";
 function Footer() {
     return (
         <div className="footer">
-            <div className="footer2 delay0">
-                <AiFillLinkedin />
+            <ul className="social">
+                <li className="social-icon">
+                    <a href="www.googe.com" className="social-link ">
+                        <AiFillLinkedin />
+                    </a>
+                </li>
+                <li className="social-icon">
+                    <a href="www.googe.com" className="social-link ">
+                        <AiFillGithub />
+                    </a>
+                </li>
+
+                <li className="social-icon">
+                    <a href="www.googe.com" className=" social-link ">
+                        <AiFillFacebook />
+                    </a>
+                </li>
+            </ul>
+            <div className="footer-svg">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <path
+                        fill="#5000ca"
+                        fill-opacity="0.18"
+                        d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+                </svg>
             </div>
-            <div className=" footer2 delay1">
-                {" "}
-                <AiFillGithub />
-            </div>
-            <div className=" footer2 delay2">
-                {" "}
-                <AiFillFacebook />
+            <div className="copyright-div">
+                <p className="copyright">©Copyright 2022, Mahamudur Rahman </p>
             </div>
         </div>
     );
