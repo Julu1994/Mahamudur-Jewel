948a5283cb7f56e8b278286387f7580da1ead8ce
julu1994 <mrjewel837@gmail.com>
2022-05-31 23:20:43 +0200

    fixing navigation bar issue

--- my-portfolio/src/components/sub-components/button.js
+++ my-portfolio/src/components/sub-components/button.js
@@ -3,7 +3,7 @@ import "./button.scss";
 function Button({ props }) {
     return (
         <div className="btn-div">
-            <a href="http://localhost:3000/" className="card-btn btn-white">
+            <a href="https://github.com/Julu1994/React_Food_Ordering_App" className="card-btn btn-white">
                 {props}
             </a>
         </div>
